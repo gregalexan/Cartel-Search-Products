17a6b356fa945d1afdf85bcf861431c359f69613
Grigorios Alexandrou <t8220004@aueb.gr>
2024-12-28 03:40:35 +0200

    Complete page product details

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -23,12 +23,47 @@ namespace Cartel_Search_Products.Controllers
             // Get the product object
             using var connection = new MySqlConnection(_configuration.GetConnectionString("Default"));
             ProductModel pm = new ProductModel(connection);
-
             product = pm.getProductById(productId);
+            connection.Close();
 
+            if (product == null)
+            {
+                return View("Details", null);
+            }
 
+            // Get the reviews of the product
+            using var connection1 = new MySqlConnection(_configuration.GetConnectionString("Default"));
+            ReviewModel rm = new ReviewModel(connection1);
+            List<Review> reviews = rm.getProductReviews(product);
+            connection1.Close();
 
-            return View("Details", product);
+            // Get the related products of this product
+            // related products are the products with the same category
+            List<String> keywords = new List<String>();
+            keywords.Add(product.category);
+            using var connection2 = new MySqlConnection(_configuration.GetConnectionString("Default"));
+            ProductModel productModel = new ProductModel(connection2);
+            List<Product> related = productModel.viewProducts(keywords, true);
+            connection2.Close();
+
+            // exclude from the related list the current product
+            foreach (var prod in related)
+            {
+                if (prod.ProductID == productId)
+                {
+                    related.Remove(prod);
+                    break;
+                }
+            }
+
+            var productDetails = new ProductDetailsModel
+            {
+                Product = product,
+                Reviews = reviews,
+                Related = related
+            };
+
+            return View("Details", productDetails);
         }
     }
 }
---
+++ Models/ProductDetailsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cartel_Search_Products.Models
+{
+    public class ProductDetailsModel
+    {
+        public Product Product { get; set; }
+        public List<Review> Reviews { get; set; }
+        public List<Product> Related {  get; set; }
+    }
+}
